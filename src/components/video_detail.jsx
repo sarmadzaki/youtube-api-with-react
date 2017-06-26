998e5f72e3a5dab5545a400dbb535a37e5d7f70d
sarmad zaki <sarmad.zaki@yahoo.com>
2017-06-26 22:19:57 +0500

    updated

--- src/components/video_detail.jsx
+++ src/components/video_detail.jsx
@@ -8,7 +8,7 @@ const VideoDetails = ({ video }) => {
   const url = `https://www.youtube.com/embed/${videoId}`;
   return (
     <div className="video-detail col-md-6">
-      <div className="embed-responsive embed-responsive-4by3">
+      <div className="embed-responsive embed-responsive-16by9">
         <iframe src={url} frameBorder="0" className="embed-responsive-item"></iframe>
       </div>
       <div className="details ">
